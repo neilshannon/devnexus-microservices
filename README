68975373ce51ea4fe5f5547a6c2c233c5c544613
neilshannon <neil@nts-dev.com>
2016-12-22 21:17:11 -0500

    initial commit

---
+++ README
@@ -0,0 +1,10 @@
+### DevNexus - Building Microservices in Java, Scala, Ruby, and node.js ###
+
+#### Java ####
+The Java portion of this project runs in Spring Boot.
+
+*To Run*
+`./gradlew bootRun`
+
+*To Test*
+`./gradlew test`
---
+++ java/src/main/java/com/ntsdev/DevnexusMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.ntsdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevnexusMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevnexusMicroserviceApplication.class, args);
+	}
+}
